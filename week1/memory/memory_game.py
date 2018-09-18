e50442a37e08c3cf65f14f5040d5132410e91f48
TeslaSama <tluo4@stevens.edu>
2018-09-18 13:06:30 -0700

    basic state changing when click

--- week1/memory/memory_game.py
+++ week1/memory/memory_game.py
@@ -1,13 +1,28 @@
 import simpleguitk as sg
+import copy
 import random
+
+FRONT = 1
+BACK = 0
 
 def new_game():
     pass
 
+# define a function from mapping the area to the index of list
+def getIndex(pos):
+    x = pos[0] // 50
+    y = pos[1] // 50
+    return x*8 + y
+
+
 #define event handlers
 def mouseclick(pos):
     #add game state logic here
-    pass
+    global number
+    index = getIndex(pos)
+    if number[index][1] == 0:
+        # the card is in the back state
+        number[index][1] = 1
 
 #cards are logically 50x50 pixels in size
 def layout():
@@ -24,8 +39,8 @@ offset = [15, 40]
 
 def init():
     global number
-    number = [i for i in range(32)]
-    tmp = number.copy()
+    number = [[i,0] for i in range(32)]
+    tmp = copy.deepcopy(number)
     tmp.extend(number)
     random.shuffle(tmp)
     print(tmp)
@@ -39,7 +54,8 @@ def draw(canvas):
         canvas.draw_line(line[0][::-1], line[1][::-1], 2, 'Blue')
     for i in range(8):
         for j in range(8):
-            canvas.draw_text(number[i*8+j], (i*50 + offset[0], j * 50 + offset[1]), 25, 'Green')
+            if number[i*8+j][1] == FRONT:
+                canvas.draw_text(number[i*8+j][0], (i*50 + offset[0], j * 50 + offset[1]), 25, 'Green')
 
 # create frame and add a button and labels
 frame = sg.create_frame("Memory", 400,400)
