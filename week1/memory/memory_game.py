eb868ad4857e792c3f24658cd39390fa992d9fe1
TeslaSama <tluo4@stevens.edu>
2018-09-18 03:11:17 -0700

    draw the board

--- week1/memory/memory_game.py
+++ week1/memory/memory_game.py
@@ -19,17 +19,33 @@ def layout():
     return lines
 
 lines = layout()
+number = []
+offset = [15, 40]
+
+def init():
+    global number
+    number = [i for i in range(32)]
+    tmp = number.copy()
+    tmp.extend(number)
+    random.shuffle(tmp)
+    print(tmp)
+    number = tmp
 
 def draw(canvas):
     global lines
+    global number
     for line in lines:
         canvas.draw_line(line[0], line[1], 2, 'Blue')
         canvas.draw_line(line[0][::-1], line[1][::-1], 2, 'Blue')
+    for i in range(8):
+        for j in range(8):
+            canvas.draw_text(number[i*8+j], (i*50 + offset[0], j * 50 + offset[1]), 25, 'Green')
 
 # create frame and add a button and labels
 frame = sg.create_frame("Memory", 400,400)
 frame.add_button("Reset", new_game, 60)
 label = frame.add_label("Turns = 0")
+init()
 
 # register event handlers
 frame.set_mouseclick_handler(mouseclick)
