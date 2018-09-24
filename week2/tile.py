4eb0c4acf8c7d2f8a58a736129a88428dac00de0
TeslaSama <tluo4@stevens.edu>
2018-09-24 13:37:45 -0700

    pile class

--- week2/tile.py
+++ week2/tile.py
@@ -6,10 +6,10 @@ TILE_HEIGHT = 100
 
 # definition of a Tile class
 class Tile:
-    def __init__(self, num, exp, pos):
+    def __init__(self, num, exp, loc):
         self.number = num
         self.exposed = exp
-        self.pos = pos
+        self.loc = loc
 
     def get_number(self):
         return self.number
@@ -27,24 +27,46 @@ class Tile:
         return "Number is " + str(self.number) + ", exposed is " + str(self.exposed)
 
     def draw_tile(self, canvas):
-        pos = self.pos
+        loc = self.loc
         if self.exposed:
-            text_location = [pos[0] + 0.2 * TILE_WIDTH, pos[1] - 0.3 * TILE_HEIGHT]
+            text_location = [loc[0] + 0.2 * TILE_WIDTH, loc[1] - 0.3 * TILE_HEIGHT]
             canvas.draw_text(str(self.number), text_location, TILE_WIDTH, 'White')
         else:
-            tile_corners = (pos, [pos[0] + TILE_WIDTH, pos[1]], [pos[0] + TILE_WIDTH, pos[1] - TILE_HEIGHT], [pos[0], pos[1] - TILE_HEIGHT])
+            tile_corners = (loc, [loc[0] + TILE_WIDTH, loc[1]], [loc[0] + TILE_WIDTH, loc[1] - TILE_HEIGHT], [loc[0], loc[1] - TILE_HEIGHT])
             canvas.draw_polygon(tile_corners, 1, "Red", "Green")
+    # selection method for tiles
+    def is_selected(self, pos):
+        loc = self.loc
+      #  if pos[0] < loc[0] + TILE_WIDTH and pos[0] > loc[0]:
+      #      if pos[1] < loc[1] and pos[1] > loc[1] - TILE_HEIGHT:
+      #          return True
+        inside_hor = loc[0] <= pos[0] <= loc[0] + TILE_WIDTH
+        inside_vert = loc[1] - TILE_HEIGHT <= pos[1] <= loc[1]
+        return inside_hor and inside_vert
+
+
+#define event handlers
+def mouseclick(pos):
+    for tile in tiles:
+        if tile.is_selected(pos):
+            if tile.is_exposed():
+                tile.hide_tile()
+            else:
+                tile.expose_tile()
+
 
 # draw handler
 def draw(canvas):
-    tile1.draw_tile(canvas)
-    tile2.draw_tile(canvas)
+    for tile in tiles:
+        tile.draw_tile(canvas)
 
 frame = simplegui.create_frame("Memory", 2 * TILE_WIDTH, TILE_HEIGHT)
 frame.set_draw_handler(draw)
+frame.set_mouseclick_handler(mouseclick)
 
 
 tile1 = Tile(3, True, [0, TILE_HEIGHT])
 tile2 = Tile(5, False, [TILE_WIDTH, TILE_HEIGHT])
+tiles = [tile1, tile2]
 
 frame.start()
