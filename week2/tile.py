16351009cfbdf4beaad53e48bea1f38400979823
TeslaSama <tluo4@stevens.edu>
2018-09-24 00:14:48 -0700

    v1_memory

--- week2/tile.py
+++ week2/tile.py
@@ -1,7 +1,15 @@
+import simpleguitk as simplegui
+
+#define globals
+TILE_WIDTH = 50
+TILE_HEIGHT = 100
+
+# definition of a Tile class
 class Tile:
-    def __init__(self, num, exp):
+    def __init__(self, num, exp, pos):
         self.number = num
         self.exposed = exp
+        self.pos = pos
 
     def get_number(self):
         return self.number
@@ -15,9 +23,23 @@ class Tile:
     def hide_tile(self):
         self.exposed = False
 
+    def __str__(self):
+        return "Number is " + str(self.number) + ", exposed is " + str(self.exposed)
 
-myTile = Tile(5)
+    def draw_tile(self, canvas):
+        if self.exposed:
+            canvas.draw_text(str(self.number), self.pos, 30, 'red')
 
-print Tile
-print myTile.number
-print myTile.get_number()
+# draw handler
+def draw(canvas):
+    tile1.draw_tile(canvas)
+    tile2.draw_tile(canvas)
+
+frame = simplegui.create_frame("Memory", 2 * TILE_WIDTH, TILE_HEIGHT)
+frame.set_draw_handler(draw)
+
+
+tile1 = Tile(3, True, [0, TILE_HEIGHT])
+tile2 = Tile(5, True, [TILE_WIDTH, TILE_HEIGHT])
+
+frame.start()
